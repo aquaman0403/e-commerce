a2fa65bf209ac0bbe53a886cdb1aa402c7471cb3
Vladimir Duck <vanducpham2k3nh@gmail.com>
2025-03-28 16:40:04 +0700

    create new product

---
+++ src/routes/product/index.js
@@ -0,0 +1,12 @@
+'use strict'
+const express = require("express");
+const productController = require("../../controllers/product.controller");
+const router = express.Router();
+const asyncHandler = require("../../helpers/asyncHandler");
+const { authenticationV2 } = require("../../auth/authUtils");
+
+router.use(authenticationV2);
+
+router.post("/", asyncHandler(productController.createProduct));
+
+module.exports = router;
